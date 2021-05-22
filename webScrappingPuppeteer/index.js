1cabe13dc2160302204f94898093a3e7b291f271
Rafael Pufal <rafael.pufal23@gmail.com>
2021-05-21 22:43:28 -0300

    scrapping and file creation basics

--- webScrappingPuppeteer/index.js
+++ webScrappingPuppeteer/index.js
@@ -1,12 +1,17 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
+
 const tableFromArray = (array) => {
   let newArray = [];
   for (let index = 0;index < 100; index++ ) {
     const newCell = array[0][index].concat(',',array[1][index])
     newArray.push(newCell);
+    //fazer um reduce com esse array p que ele me entregue uma string
   }
-  return newArray;
+  let textBase = 'Game Title, MetaScore\n';
+  textBase = newArray.reduce((accumulator, currentValue) => accumulator + currentValue + '\n', textBase)
+  console.log(textBase);
+  return textBase;
 }
 
 (async () => {
@@ -14,7 +19,6 @@ const tableFromArray = (array) => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://www.metacritic.com/browse/games/score/metascore/all/switch/filtered');
-  // await page.screenshot({ path: 'exampleZelda.png' });
 
   const result = await page.evaluate(()=>{
     let titlesFromWeb = document.querySelectorAll('a.title h3');
@@ -27,5 +31,6 @@ const tableFromArray = (array) => {
   })
   await browser.close();
   const singleTable = tableFromArray(result)
-  fs.writeFileSync('./tables/gamesData.csv', JSON.stringify(singleTable), 'utf-8');
+  fs.writeFileSync('./tables/gamesData.csv', singleTable, 'utf-8');
+
 })();
