5dcf90a90e17a3d8931f6b67dfdf6e970f3499d2
Rafael Pufal <rafael.pufal23@gmail.com>
2021-05-23 14:21:10 -0300

    multiple data points collected with new func

--- webScrappingPuppeteer/index.js
+++ webScrappingPuppeteer/index.js
@@ -6,12 +6,13 @@ const fs = require('fs');
 const tableFromArray = (array, nColumns) => {
   let newArray = [];
   for (let index = 0;index < array[0].length; index++ ) {
+    let newCell = array[0][index];
     for (let index2 = 1; index2 < nColumns; index2++) {
-      const newCell = array[0][index].concat(',',array[index2][index])
-      newArray.push(newCell);
+      newCell = newCell.concat(',',array[index2][index])
     }
+    newArray.push(newCell);
   }
-  let textBase = 'Game Title, MetaScore\n';
+  let textBase = '';
   textBase = newArray.reduce((accumulator, currentValue) => accumulator + currentValue + '\n', textBase)
   console.log(textBase);
   return textBase;
@@ -46,12 +47,24 @@ const getInfos  = (requestInfos, queriesResults) =>{
   const result = await page.evaluate(()=>{
     let queriesResults = [];
     const requestInfos = [{
-      title: 'game title',
+      title: 'game-title',
       query: 'a.title h3'
     },
     {
       title: 'metascore',
-      query: 'td.clamp-summary-wrap div.clamp-score-wrap a.metascore_anchor div.metascore_w.large.game.positive'
+      query: 'td.clamp-summary-wrap div.clamp-score-wrap a.metascore_anchor div.metascore_w.large.game'
+    },
+    {
+      title: 'userscore',
+      query: 'div.metascore_w.user.large.game'
+    },
+    {
+      title: 'platform',
+      query: 'div.platform'
+    },
+    {
+      title: 'release-date',
+      query: 'div.clamp-details span:not(.label):not(.data)'
     }
     ]
     requestInfos.map((info) => {
@@ -63,6 +76,7 @@ const getInfos  = (requestInfos, queriesResults) =>{
     return queriesResults;
   })
   await browser.close();
-  const finalTable = tableFromArray(result,2)
+  const finalTable = tableFromArray(result,result.length)
+  // const finalTable = tableFromArray(result,2)
   fs.writeFileSync('./tables/gamesData.csv', finalTable, 'utf-8');
 })();
