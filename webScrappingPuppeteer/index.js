373339f57dc2a865c0a7f9b0055efe4b02c41fc8
Rafael Pufal <rafael.pufal23@gmail.com>
2021-05-25 22:44:46 -0300

    all tables ready

--- webScrappingPuppeteer/index.js
+++ webScrappingPuppeteer/index.js
@@ -5,12 +5,22 @@ const fs = require('fs');
 
 const tableFromArray = (array, nColumns) => {
   let newArray = [];
-  for (let index = 0;index < array[0].length; index++ ) {
-    let newCell = array[0][index];
-    for (let index2 = 1; index2 < nColumns; index2++) {
-      newCell = newCell.concat(',',array[index2][index])
+  if (nColumns > 1) {
+    for (let index = 0;index < array[0].length; index++ ) {
+      let newCell = array[0][index];
+      for (let index2 = 1; index2 < nColumns; index2++) {
+        newCell = newCell.concat(',',array[index2][index])
+      }
+      newArray.push(newCell);
     }
-    newArray.push(newCell);
+  } else if (nColumns === 1) {
+    for (let index = 0;index < array.length; index++ ) {
+      let newCell = array[index];
+      for (let index2 = 1; index2 < nColumns; index2++) {
+        newCell = newCell.concat(',',array[index2][index])
+      }
+      newArray.push(newCell);
+    }
   }
   let textBase = '';
   textBase = newArray.reduce((accumulator, currentValue) => accumulator + currentValue + '\n', textBase)
@@ -18,76 +28,114 @@ const tableFromArray = (array, nColumns) => {
   return textBase;
 }
 
-
-// const pagesToVisit = ['https://www.metacritic.com/browse/games/score/metascore/all/switch/filtered',
-//   'https://www.metacritic.com/browse/games/score/metascore/all/xboxone/filtered'];
-const pagesToVisit = [
+const pagesToVisitLegacy = [
   {
-    pageTitle: 'SwitchBest1',
+    pageTitle: 'ps3',
+    url: 'https://www.metacritic.com/browse/games/score/metascore/all/ps3/filtered'
+  },
+  {
+    pageTitle: 'xbox360',
+    url: 'https://www.metacritic.com/browse/games/score/metascore/all/xbox360/filtered'
+  },
+  {
+    pageTitle: 'ds',
+    url: 'https://www.metacritic.com/browse/games/score/metascore/all/ds/filtered'
+  },
+  {
+    pageTitle: '3ds',
+    url: 'https://www.metacritic.com/browse/games/score/metascore/all/3ds/filtered'
+  },
+  {
+    pageTitle: 'wii',
+    url: 'https://www.metacritic.com/browse/games/score/metascore/all/wii/filtered'
+  },
+  {
+    pageTitle: 'wiiu',
+    url: 'https://www.metacritic.com/browse/games/score/metascore/all/wii-u/filtered'
+  },
+];
+
+const pagesToVisitCurrent = [
+  {
+    pageTitle: 'switch',
     url: 'https://www.metacritic.com/browse/games/score/metascore/all/switch/filtered'
   },
   {
-    pageTitle: 'XboxBest1',
+    pageTitle: 'xboxone',
     url: 'https://www.metacritic.com/browse/games/score/metascore/all/xboxone/filtered'
+  },
+  {
+    pageTitle: 'ps4',
+    url: 'https://www.metacritic.com/browse/games/score/metascore/all/ps4/filtered'
+  },
+  {
+    pageTitle: 'pc',
+    url: 'https://www.metacritic.com/browse/games/score/metascore/all/pc/filtered'
   }
 ]
 
-const metacriticScrapper = async (pagesToVisit) => {
+
+const metacriticScrapper = async (pagesToVisit, nPages) => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  for (let index = 0; index < pagesToVisit.length; index++ ) {
-    const currentPage = pagesToVisit[index].url;
-    await page.goto(currentPage);
-    const result = await page.evaluate(()=>{
-      let queriesResults = [];
-      const requestInfos = [{
-        title: 'game-title',
-        query: 'a.title h3'
-      },
-      {
-        title: 'metascore',
-        query: 'td.clamp-summary-wrap div.clamp-score-wrap a.metascore_anchor div.metascore_w.large.game'
-      },
-      {
-        title: 'userscore',
-        query: 'div.metascore_w.user.large.game'
-      },
-      {
-        title: 'platform',
-        query: 'div.platform span.data'
-      },
-      {
-        title: 'release-date',
-        query: 'div.clamp-details span:not(.label):not(.data)'
-      },
-      {
-        title: 'metacritic-website',
-        query: 'a.title'
-      },
-      {
-        title: 'thumbnail',
-        query: 'td.clamp-image-wrap a img'
-      }
-      ];
-      requestInfos.map((info) => {
-        let infosFromWeb = document.querySelectorAll(info.query);
-        const infosList = [...infosFromWeb];
-        if (info.title === 'metacritic-website') {
-          const infos = infosList.map(currData => currData.getAttribute('href'));
-          queriesResults.push(infos);
-        } else if (info.title === 'thumbnail') {
-          const infos = infosList.map(currData => currData.getAttribute('src'));
-          queriesResults.push(infos);
-        } else {
-          const infos = infosList.map(currData => currData.innerText);
-          queriesResults.push(infos);
+  for (let listIndex = 0; listIndex < pagesToVisit.length; listIndex++ ) {
+    for (let pageIndex = 0; pageIndex < nPages; pageIndex++) {
+      const currentPage = `${pagesToVisit[listIndex].url}?page=${pageIndex}`;
+      await page.goto(currentPage);
+      const result = await page.evaluate(()=>{
+        let queriesResults = [];
+        const requestInfos = [{
+          title: 'game-title',
+          query: 'a.title h3'
+        },
+        {
+          title: 'metascore',
+          query: 'td.clamp-summary-wrap div.clamp-score-wrap a.metascore_anchor div.metascore_w.large.game'
+        },
+        {
+          title: 'userscore',
+          query: 'div.metascore_w.user.large.game'
+        },
+        {
+          title: 'platform',
+          query: 'div.platform span.data'
+        },
+        {
+          title: 'release-date',
+          query: 'div.clamp-details span:not(.label):not(.data)'
+        },
+        {
+          title: 'metacritic-website',
+          query: 'a.title'
+        },
+        {
+          title: 'thumbnail',
+          query: 'td.clamp-image-wrap a img'
         }
+        ];
+        requestInfos.map((info) => {
+          let infosFromWeb = document.querySelectorAll(info.query);
+          const infosList = [...infosFromWeb];
+          if (info.title === 'metacritic-website') {
+            const infos = infosList.map(currData => currData.getAttribute('href'));
+            queriesResults.push(infos);
+          } else if (info.title === 'thumbnail') {
+            const infos = infosList.map(currData => currData.getAttribute('src'));
+            queriesResults.push(infos);
+          } else {
+            const infos = infosList.map(currData => currData.innerText);
+            queriesResults.push(infos);
+          }
+        })
+        return queriesResults;
       })
-      return queriesResults;
-    })
-    const finalTable = tableFromArray(result,result.length)
-    fs.writeFileSync(`./tables/${pagesToVisit[index].pageTitle}.csv`, finalTable, 'utf-8');
+      const finalTable = tableFromArray(result,result.length);
+      fs.writeFileSync(`./tables_best_games/${pagesToVisit[listIndex].pageTitle}/${pagesToVisit[listIndex].pageTitle}${pageIndex}.csv`, finalTable, 'utf-8');
+      const urlTable = tableFromArray(result[result.length - 2],1);
+      fs.writeFileSync(`./tables_best_games/${pagesToVisit[listIndex].pageTitle}/url/${pagesToVisit[listIndex].pageTitle}${pageIndex}-url.csv`, urlTable, 'utf-8');
+    }
   }
   await browser.close();  
 };
-metacriticScrapper(pagesToVisit);
+metacriticScrapper(pagesToVisitCurrent, 3);
+metacriticScrapper(pagesToVisitLegacy, 3);
